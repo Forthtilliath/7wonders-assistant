91d2c6cce660f0df42e760b313c0268c5f8fdeda
Forthtilliath <forth@live.fr>
2023-10-05 20:28:28 +0200

    ✨ feat: remove desc data & Medatadata type

--- src/data/app.ts
+++ src/data/app.ts
@@ -1,6 +1,3 @@
-export const METADATA: Metadata = {
+export const METADATA = {
   title: '7 Wonders: Assistant',
-  description:
-    '7 Wonders: Assistant is an application that helps users manage and calculate the scores of their 7 Wonders games.',
-  // description: "7 Wonders: Assistant est une application qui aide les utilisateurs à gérer et à calculer les scores de leurs parties de 7 Wonders.",
 };
