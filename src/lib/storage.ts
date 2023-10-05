c98f835a8bd3819744661c1bb307c1fff68e1ce6
Forthtilliath <forth@live.fr>
2023-10-06 01:10:20 +0200

    ✨ feat: move defaultPlayers

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,5 +1,22 @@
+const defaultPlayers: Player[] = [
+  {
+    id: '349a799f-a55d-44b3-1efa-e309e77d67ec',
+    name: 'Mike',
+    isFavorite: 'true',
+    avatar: '/assets/images/logo-mike.png',
+  },
+  {
+    id: 'efbedc67-2ae7-4b4d-0980-ca7d7b858bd2',
+    name: 'Forth',
+    isFavorite: 'false',
+    avatar: '/assets/images/logo-forth.png',
+  },
+];
+
 export function getPlayers(): Player[] {
-  return JSON.parse(localStorage.getItem('players') ?? '[]');
+  const LS = localStorage.getItem('players');
+
+  return LS ? JSON.parse(LS) : defaultPlayers;
 }
 
 export function addPlayer(player: Player) {
