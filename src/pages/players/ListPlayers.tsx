5e6c738eee9a784d5c2e452832bb80897a90dda1
Forthtilliath <forth@live.fr>
2023-10-06 01:10:20 +0200

    ✨ feat: move defaultPlayers

--- src/pages/players/ListPlayers.tsx
+++ src/pages/players/ListPlayers.tsx
@@ -1,25 +1,9 @@
+import { CardNewPlayer } from '@/components/cards/CardNewPlayer';
 import { CardPlayer } from '@/components/cards/CardPlayer';
-import { FaPlus } from '@/components/shared/Icons';
-import { useLocalStorage } from '@/hooks/useLocalStorage';
-import { Link } from 'react-router-dom';
-
-const testValues: Player[] = [
-  {
-    id: '349a799f-a55d-44b3-1efa-e309e77d67ec',
-    name: 'Mike',
-    isFavorite: 'true',
-    avatar: '/assets/images/logo-mike.png',
-  },
-  {
-    id: 'efbedc67-2ae7-4b4d-0980-ca7d7b858bd2',
-    name: 'Forth',
-    isFavorite: 'false',
-    avatar: '/assets/images/logo-forth.png',
-  },
-];
+import { usePlayers } from '@/hooks/usePlayers';
 
 export default function ListPlayers() {
-  const [players] = useLocalStorage<Player[]>('players', testValues);
+  const [players] = usePlayers();
 
   return (
     <main className="mx-auto grid max-w-[800px] grid-cols-3 gap-2 p-4">
@@ -27,11 +11,7 @@ export default function ListPlayers() {
         <CardPlayer key={player.id} {...player} />
       ))}
 
-      <Link
-        to="/players/new"
-        className="flex aspect-square h-auto max-w-[250px] items-center justify-center rounded bg-slate-700">
-        <FaPlus size={'5rem'} />
-      </Link>
+      <CardNewPlayer />
     </main>
   );
 }
