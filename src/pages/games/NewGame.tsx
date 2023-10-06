0e865f9d8f4cfbe57dea096417cecd36c47cee4a
Forthtilliath <forth@live.fr>
2023-10-06 05:09:37 +0200

    ✨ feat: update component
    - add favorite players into new game per default
    - remove player if he's already in game
    - display default slot if the slot is empty
    - change number of columns from 3 to 4

--- src/pages/games/NewGame.tsx
+++ src/pages/games/NewGame.tsx
@@ -1,18 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { CardNewPlayer } from '@/components/cards/CardNewPlayer';
 import { CardPlayerNewGame } from '@/components/cards/CardPlayerNewGame';
+import { GiMeeple } from '@/components/shared/Icons';
 import { usePlayers } from '@/hooks/usePlayers';
 
 const MAX_PLAYERS = 7;
 
 export default function NewGame() {
-  const [playersInGame, setPlayersInGame] = useState<Player[]>([]);
   const [players] = usePlayers();
+  const [playersInGame, setPlayersInGame] = useState<Player[]>([]);
+
+  useEffect(() => {
+    const favoritePlayers = players.filter(p => p.isFavorite === 'true').slice(0, MAX_PLAYERS);
+    setPlayersInGame(favoritePlayers)
+  }, [players])
 
   const addPlayerIntoTheGame = (player: Player) => () => {
+    if (playersInGame.includes(player)) {
+      removePlayerFromTheGame(player.id)();
+      return;
+    }
     if (playersInGame.length >= MAX_PLAYERS) return;
-    if( playersInGame.includes(player) ) return;
 
     setPlayersInGame((p) => [...p, player]);
   };
@@ -21,10 +30,14 @@ export default function NewGame() {
     setPlayersInGame((p) => p.filter((player) => player.id !== playerId));
   };
 
+  const emptyPlayers = Array.from({
+    length: MAX_PLAYERS - playersInGame.length,
+  });
+
   return (
     <section>
       <header className="h-60 bg-wonders-blue">
-        <main className="mx-auto grid max-w-[800px] grid-cols-3 gap-2 p-4">
+        <main className="mx-auto grid max-w-[800px] grid-cols-4 gap-2 p-4">
           {playersInGame.map((player) => (
             <CardPlayerNewGame
               key={player.id}
@@ -32,22 +45,23 @@ export default function NewGame() {
               onClick={removePlayerFromTheGame(player.id)}
             />
           ))}
+          {emptyPlayers.map((_,i) => (
+            <div key={i} className="flex aspect-square w-full items-center justify-center bg-wonders-blue-dark transition-all">
+              <GiMeeple size={'3rem'} />
+            </div>
+          ))}
         </main>
       </header>
 
       <main className="mx-auto grid max-w-[800px] grid-cols-3 gap-2 p-4">
-        {players.map((player) => {
-          const inGame = playersInGame.some((p) => p.id === player.id);
-          return (
-            <CardPlayerNewGame
-              key={player.id}
-              {...player}
-              inGame={inGame}
-              onClick={addPlayerIntoTheGame(player)}
-              // onClick={inGame ? () => {} : addPlayerIntoTheGame(player)}
-            />
-          );
-        })}
+        {players.map((player) => (
+          <CardPlayerNewGame
+            key={player.id}
+            {...player}
+            inGame={playersInGame.some((p) => p.id === player.id)}
+            onClick={addPlayerIntoTheGame(player)}
+          />
+        ))}
 
         <CardNewPlayer />
       </main>
