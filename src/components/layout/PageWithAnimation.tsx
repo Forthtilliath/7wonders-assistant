c45bb88a6e1496a9b7a4c01f7c7c7b075882cd39
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-10-06 23:39:39 +0200

    🐛 fix: correct the overflow which hidden header options icons

--- src/components/layout/PageWithAnimation.tsx
+++ src/components/layout/PageWithAnimation.tsx
@@ -13,7 +13,7 @@ const PageWithAnimation = ({ children }: React.PropsWithChildren) => {
         initial={{ opacity: 0, x: '100%' }}
         animate={{ opacity: 1, x: '0' }}
         exit={{ opacity: 0, x: '-100%' }}
-        // transition={{ duration: 0.3 }}
+        transition={{ duration: 0.3 }}
         // transition={{
         //   type: "spring",
         //   stiffness: 260,
--- src/pages/games/NewGame.tsx
+++ src/pages/games/NewGame.tsx
@@ -1,18 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { CardNewPlayer } from '@/components/cards/CardNewPlayer';
 import { CardPlayerNewGame } from '@/components/cards/CardPlayerNewGame';
+import { HeaderOptions } from '@/components/layout/HeaderOptions';
+import { ButtonIcon } from '@/components/shared/ButtonIcon';
+import { BsCheckLg, GiMeeple } from '@/components/shared/Icons';
 import { usePlayers } from '@/hooks/usePlayers';
 
+const MIN_PLAYERS = 2;
 const MAX_PLAYERS = 7;
 
 export default function NewGame() {
-  const [playersInGame, setPlayersInGame] = useState<Player[]>([]);
   const [players] = usePlayers();
+  const [playersInGame, setPlayersInGame] = useState<Player[]>([]);
+
+  useEffect(() => {
+    const favoritePlayers = players
+      .filter((p) => p.isFavorite === 'true')
+      .slice(0, MAX_PLAYERS);
+    setPlayersInGame(favoritePlayers);
+  }, [players]);
 
   const addPlayerIntoTheGame = (player: Player) => () => {
+    if (playersInGame.includes(player)) {
+      removePlayerFromTheGame(player.id)();
+      return;
+    }
     if (playersInGame.length >= MAX_PLAYERS) return;
-    if( playersInGame.includes(player) ) return;
 
     setPlayersInGame((p) => [...p, player]);
   };
@@ -21,10 +35,21 @@ export default function NewGame() {
     setPlayersInGame((p) => p.filter((player) => player.id !== playerId));
   };
 
+  const launchGame = () => {
+    console.log(playersInGame)
+  }
+
+  const emptyPlayers = Array.from({
+    length: MAX_PLAYERS - playersInGame.length,
+  });
+
   return (
-    <section>
-      <header className="h-60 bg-wonders-blue">
-        <main className="mx-auto grid max-w-[800px] grid-cols-3 gap-2 p-4">
+    <section className='overflow-y-auto h-full'>
+      <HeaderOptions>
+        {playersInGame.length >= MIN_PLAYERS && <ButtonIcon icon={BsCheckLg} onClick={launchGame} type="button" />}
+      </HeaderOptions>
+      <header className="bg-wonders-blue">
+        <main className="mx-auto grid max-w-[800px] grid-cols-4 gap-2 p-4">
           {playersInGame.map((player) => (
             <CardPlayerNewGame
               key={player.id}
@@ -32,22 +57,25 @@ export default function NewGame() {
               onClick={removePlayerFromTheGame(player.id)}
             />
           ))}
+          {emptyPlayers.map((_, i) => (
+            <div
+              key={i}
+              className="flex aspect-square w-full items-center justify-center bg-wonders-blue-dark transition-all">
+              <GiMeeple size={'60%'} />
+            </div>
+          ))}
         </main>
       </header>
 
-      <main className="mx-auto grid max-w-[800px] grid-cols-3 gap-2 p-4">
-        {players.map((player) => {
-          const inGame = playersInGame.some((p) => p.id === player.id);
-          return (
-            <CardPlayerNewGame
-              key={player.id}
-              {...player}
-              inGame={inGame}
-              onClick={addPlayerIntoTheGame(player)}
-              // onClick={inGame ? () => {} : addPlayerIntoTheGame(player)}
-            />
-          );
-        })}
+      <main className="mx-auto grid max-w-[800px] auto-rows-min grid-cols-3 gap-2 p-4 overflow-y-auto h-full">
+        {players.map((player) => (
+          <CardPlayerNewGame
+            key={player.id}
+            {...player}
+            inGame={playersInGame.some((p) => p.id === player.id)}
+            onClick={addPlayerIntoTheGame(player)}
+          />
+        ))}
 
         <CardNewPlayer />
       </main>
