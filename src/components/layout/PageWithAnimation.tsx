6fae0f789c14d1f8aa5b23db278df6098490549c
Forthtilliath <forth@live.fr>
2023-10-06 05:03:56 +0200

    ✨ feat: add overflow & transition duration

--- src/components/layout/PageWithAnimation.tsx
+++ src/components/layout/PageWithAnimation.tsx
@@ -13,13 +13,13 @@ const PageWithAnimation = ({ children }: React.PropsWithChildren) => {
         initial={{ opacity: 0, x: '100%' }}
         animate={{ opacity: 1, x: '0' }}
         exit={{ opacity: 0, x: '-100%' }}
-        // transition={{ duration: 0.3 }}
+        transition={{ duration: 0.3 }}
         // transition={{
         //   type: "spring",
         //   stiffness: 260,
         //   damping: 20,
         // }}
-        className="flex-grow">
+        className="flex-grow overflow-auto">
         {children}
       </motion.div>
     </AnimatePresence>
