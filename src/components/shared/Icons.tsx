cf6686a09ee87791de8866b3fc036d3dbf097faf
Forthtilliath <forth@live.fr>
2023-10-07 21:15:20 +0200

    ✨ feat: add arrow up and down icons

--- src/components/shared/Icons.tsx
+++ src/components/shared/Icons.tsx
@@ -343,6 +343,41 @@ export function BsCheckLg({ size = '1rem', ...props }: IconProps) {
   );
 }
 
+export function BsFillFileArrowDownFill({
+  size = '1rem',
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 16 16"
+      height={size}
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}>
+      <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM8 5a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5A.5.5 0 0 1 8 5z" />
+    </svg>
+  );
+}
+
+export function BsFillFileArrowUpFill({ size = '1rem', ...props }: IconProps) {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 16 16"
+      height={size}
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}>
+      <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM7.5 6.707 6.354 7.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707z" />
+    </svg>
+  );
+}
+
 export function BsTrash3Fill({ size = '1rem', ...props }: IconProps) {
   return (
     <svg
