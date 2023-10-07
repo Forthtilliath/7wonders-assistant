1c1515d95482176a6e0f92dc864212e77be80653
Forthtilliath <forth@live.fr>
2023-10-07 02:15:59 +0200

    ✨ feat: add game store and setPlayers & setScore

--- src/lib/gameStore.ts
+++ src/lib/gameStore.ts
@@ -1,15 +1,66 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
+
+function produce<T>(cb: (state: T) => void) {
+  return (state: T) => {
+    const copy = { ...state };
+    cb(copy);
+    return copy;
+  };
+}
+
+type Score = Record<string, number>;
+
+type Category =
+  | 'military'
+  | 'treasury'
+  | 'wonders'
+  | 'civilians'
+  | 'scientifics'
+  | 'commercials'
+  | 'guilds'
+  | 'armada'
+  | 'leaders'
+  | 'cities';
 
 interface State {
   players: Player[];
   setPlayers: (players: Player[]) => void;
+
+  scores: Record<Category, Score>;
+  setScore: (category: Category, idPlayer: string, score: number) => void;
 }
 
 export const useGameStore = create<State>()(
-  devtools((set) => ({
-    players: [],
-    setPlayers: (players) => set(() => ({ players }), undefined, 'SET_PLAYERS'),
-    // increase: (by) => set((state) => ({ bears: state.bears + by })),
-  }))
+  devtools(
+    persist(
+      (set) => ({
+        players: [],
+        setPlayers: (players) =>
+          set(() => ({ players }), undefined, 'SET_PLAYERS'),
+
+        scores: {
+          military: {},
+          treasury: {},
+          wonders: {},
+          civilians: {},
+          scientifics: {},
+          commercials: {},
+          guilds: {},
+          armada: {},
+          leaders: {},
+          cities: {},
+        },
+        setScore: (category, idPlayer, score) =>
+          set(
+            produce((state) => {
+              state.scores[category][idPlayer] = score;
+            }),
+            undefined,
+            'SET_SCORE'
+          ),
+      }),
+      { name: 'GAME_STORE' }
+    )
+  )
 );
--- src/pages/games/scores/Treasury.tsx
+++ src/pages/games/scores/Treasury.tsx
@@ -6,14 +6,14 @@ import { ButtonIcon } from '@/components/shared/ButtonIcon';
 import { AiOutlineArrowRight } from '@/components/shared/Icons';
 import { useGameStore } from '@/lib/gameStore';
 
-export function Military() {
+export function Treasury() {
   const navigate = useNavigate();
   const { players, scores, setScore } = useGameStore();
 
-  const nextStep = () => navigate('/scores/treasury');
+  const nextStep = () => navigate('/scores/wonders');
 
   const saveScore: SaveScoreEventHandler = (idPlayer: string) => (e) =>
-    setScore('military', idPlayer, e.currentTarget.valueAsNumber);
+    setScore('treasury', idPlayer, e.currentTarget.valueAsNumber);
 
   return (
     <section>
@@ -21,7 +21,7 @@ export function Military() {
         <ButtonIcon
           icon={AiOutlineArrowRight}
           onClick={nextStep}
-          className="text-white"
+          className="text-wonders-blue"
         />
       </HeaderOptions>
       <div className="flex flex-col gap-4 px-10 py-6">
@@ -30,7 +30,7 @@ export function Military() {
             <CardPlayerInGame {...player} className="w-[100px]" />
             <input
               type="number"
-              defaultValue={scores['military'][player.id] ?? 0}
+              defaultValue={scores['treasury'][player.id] ?? 0}
               onChange={saveScore(player.id)}
               className="border-wonders-dark w-full rounded border-2 bg-slate-900 p-3 text-center"
             />
