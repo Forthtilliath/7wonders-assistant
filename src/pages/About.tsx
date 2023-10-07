657b1dfc426607e346393d46e107dd46359fd553
Forthtilliath <forth@live.fr>
2023-10-07 19:31:03 +0200

    🔧 feat: optimize images and update code
    - Optimize images to improve performance
    - Update code to follow best practices

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,7 @@
-import forth from '@/assets/logo-forth.png';
-import mike from '@/assets/logo-mike.png';
-import logo from '@/assets/logo-wonder.png';
+import forth from '@/assets/devs/logo-forth.webp';
+import mike from '@/assets/devs/logo-mike.webp';
+import logo_mobile from '@/assets/logo-wonders-mobile.webp';
+import logo_tablet from '@/assets/logo-wonders-tablet.webp';
 import { CardDev } from '@/components/cards/CardDev';
 
 export default function About() {
@@ -9,7 +10,20 @@ export default function About() {
   return (
     <main className="flex h-full flex-col content-center items-center justify-evenly">
       <section className="flex flex-col gap-4 px-4 text-center text-gray-400">
-        <img src={logo} alt="7 Wonders" className="max-w-[400px] w-full" />
+        <picture>
+          <source
+            media="(max-width: 767px)"
+            srcSet={logo_mobile}
+          />
+          <source
+            media="(min-width: 768px)"
+            srcSet={logo_tablet}
+          />
+          <img
+            src={logo_mobile}
+            alt="7 Wonders Logo"
+          />
+        </picture>
         <h2>7 Wonders: Assistant</h2>
         <p>Version {version}</p>
       </section>
