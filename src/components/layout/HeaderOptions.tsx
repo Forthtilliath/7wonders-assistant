e5f330cff96f9a4a08369e50d2911c3883e452d1
Forthtilliath <forth@live.fr>
2023-10-07 03:20:26 +0200

    💄 style: reduce margin between icones for small screens

--- src/components/layout/HeaderOptions.tsx
+++ src/components/layout/HeaderOptions.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export function HeaderOptions({ children }: PropsWithChildren) {
   return (
-    <div className="fixed -top-16 right-0 flex h-16 items-center text-wonders-yellow">
+    <div className="fixed -top-16 right-0 flex h-16 items-center text-wonders-yellow mr-2">
       {children}
     </div>
   );
