7d69eb13c203b00e11d245eba05b6b208435b32d
Forthtilliath <forth@live.fr>
2023-10-07 03:21:26 +0200

    ✨ feat: add categories const

--- src/data/app.ts
+++ src/data/app.ts
@@ -1,3 +1,16 @@
 export const METADATA = {
   title: '7 Wonders: Assistant',
 };
+
+export const CATEGORIES = [
+  'military',
+  'treasury',
+  'wonders',
+  'civilians',
+  'scientifics',
+  'commercials',
+  'guilds',
+  'armada',
+  'leaders',
+  'cities',
+] as const;
