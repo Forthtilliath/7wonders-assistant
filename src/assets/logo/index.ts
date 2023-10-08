0e95192647cda848a92cd276fc7858f48c1d316b
Forthtilliath <forth@live.fr>
2023-10-08 03:07:55 +0200

    ✨ feat: add indexes

---
+++ src/assets/logo/index.ts
@@ -0,0 +1,4 @@
+import logo_mobile from './logo-wonders-mobile.webp';
+import logo_tablet from './logo-wonders-tablet.webp';
+
+export { logo_mobile, logo_tablet };
---
+++ src/components/cards/index.ts
@@ -0,0 +1,5 @@
+export { CardDev } from './CardDev';
+export { CardNewPlayer } from './CardNewPlayer';
+export { CardPlayer } from './CardPlayer';
+export { CardPlayerInGame } from './CardPlayerInGame';
+export { CardPlayerNewGame } from './CardPlayerNewGame';
