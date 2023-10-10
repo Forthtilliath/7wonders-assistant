1506250b616c0fc13726eb6bae5114c9aa708f36
Forthtilliath <forth@live.fr>
2023-10-10 17:35:31 +0200

    ⚡️ refactor: upgrade performances
    - change motion to m
    - change AnimationPresence to LazyMotion

--- src/components/layout/PageWithAnimation.tsx
+++ src/components/layout/PageWithAnimation.tsx
@@ -1,25 +1,20 @@
-import { AnimatePresence, motion } from 'framer-motion';
 import { useLocation } from 'react-router-dom';
+import { domAnimation, LazyMotion, m } from 'framer-motion';
 
 export const PageWithAnimation = ({ children }: React.PropsWithChildren) => {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode="popLayout" initial={false}>
-      <motion.div
+    <LazyMotion features={domAnimation}>
+      <m.div
         key={location.pathname}
         initial={{ opacity: 0, x: '100%' }}
         animate={{ opacity: 1, x: '0' }}
         exit={{ opacity: 0, x: '-100%' }}
         transition={{ duration: 0.3 }}
-        // transition={{
-        //   type: "spring",
-        //   stiffness: 260,
-        //   damping: 20,
-        // }}
         className="flex-grow">
         {children}
-      </motion.div>
-    </AnimatePresence>
+      </m.div>
+    </LazyMotion>
   );
 };
