7f072eb034bd1ca33debecf10f409822d1881edd
Forthtilliath <forth@live.fr>
2023-10-10 17:33:06 +0200

    ✨ feat: add indexes

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,3 +1,5 @@
 export { ButtonIcon } from './ButtonIcon';
 export { ButtonToggleIcon } from './ButtonToggleIcon';
 export { GroupInputs } from './GroupInputs';
+export { IconOnly } from './IconOnly';
+// Je n'ai pas mis Icon pour que ce soit plus clair quand on l'importe
