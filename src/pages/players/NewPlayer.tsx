f9bac31b1068f915fda6e19870a8d6ee678ef591
Forthtilliath <forth@live.fr>
2023-10-11 16:40:35 +0200

    🖌️ refactor: update imports & use InputPlayer

--- src/pages/players/NewPlayer.tsx
+++ src/pages/players/NewPlayer.tsx
@@ -1,22 +1,20 @@
 import { useEffect, useRef } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-
-import * as LS from '@/lib/storage';
-import type { Player } from '@/@types/storage';
-import { HeaderOptions } from '@/components/layout/HeaderOptions';
-import { ButtonIcon } from '@/components/shared/ButtonIcon';
-import { ButtonToggleIcon } from '@/components/shared/ButtonToggleIcon';
 import {
   BiArchiveIn,
   BiSolidArchiveOut,
   BsCheckLg,
-  BsPencilFill,
   GiMeeple,
   ImStarEmpty,
   ImStarFull,
 } from '@/components/shared/Icons';
-import { generateUuidv4 } from '@/helpers/string';
-import { useToggle } from '@/hooks/useToggle';
+import type { Player } from '@types';
+import { HeaderOptions } from '@components/layout/HeaderOptions';
+import { ButtonIcon, ButtonToggleIcon } from '@components/shared';
+import { InputPlayer } from '@components/ui';
+import { generateUuidv4 } from '@helpers';
+import { LS } from '@lib';
+import { useToggle } from '@hooks';
 
 export default function NewPlayer() {
   const navigate = useNavigate();
@@ -64,24 +62,14 @@ export default function NewPlayer() {
           />
         </HeaderOptions>
 
-        <div className="relative mb-3 flex w-full flex-wrap items-stretch">
-          <BsPencilFill className="absolute z-10 h-full w-8 items-center justify-center rounded py-3 pl-3 text-center text-base font-normal leading-snug text-slate-600" />
-          <input
-            ref={inputRef}
-            type="text"
-            placeholder="Name"
-            className="w-full p-4 pl-10 text-slate-600 placeholder-slate-300"
-            name="name"
-            required
-          />
-        </div>
+        <InputPlayer ref={inputRef} />
 
         {/* NOTE: Si on change de page, on perd le form ! */}
         {/* Solution possible : Sidebar / Modal */}
         <p className="mt-10 text-center text-lg">Avatar</p>
         <NavLink className="mx-auto mt-3 block w-[200px]" to="#">
           <div className="flex h-[200px] w-full items-center justify-center bg-wonders-blue">
-            <GiMeeple size={'5rem'} />
+            <GiMeeple size="5rem" />
           </div>
           <div className="flex h-16 w-full items-center justify-center bg-wonders-blue-dark text-lg">
             Change
