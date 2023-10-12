271ad05c59bdb3d15b455dcf1d01dc1eff650ead
Forthtilliath <forth@live.fr>
2023-10-12 17:05:10 +0200

    🔥 feat: remove usePlayer hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
 export { useLocalStorage } from './useLocalStorage';
 export { usePhotoGallery } from './usePhotoGallery';
-export { usePlayer } from './usePlayer';
 export { usePlayers } from './usePlayers';
 export { useToggle } from './useToggle';
