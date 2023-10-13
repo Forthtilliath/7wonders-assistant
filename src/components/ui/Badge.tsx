f604fc3c331bf6223540de0e0aa3889aaede4ab2
Forthtilliath <forth@live.fr>
2023-10-14 01:06:50 +0200

    ♻️ refactor: change children into label

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -1,16 +1,17 @@
-import { PropsWithChildren } from 'react';
 import { cn } from '@/helpers';
 
-type Props = PropsWithChildren<PropsWithClassname>;
+type Props = PropsWithClassname<{
+  label: string
+}>;
 
-export function Badge({ children, className }: Props) {
+export function Badge({ label, className }: Props) {
   return (
     <span
       className={cn(
         'mr-2 rounded bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-300',
         className
       )}>
-      {children}
+      {label}
     </span>
   );
 }
