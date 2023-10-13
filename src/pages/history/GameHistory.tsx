4c21a27e63c3790b2f5e5c1460e75e2bc497369d
Forthtilliath <forth@live.fr>
2023-10-13 13:55:58 +0200

    :sparkles: feat: add array to list scores

--- src/pages/history/GameHistory.tsx
+++ src/pages/history/GameHistory.tsx
@@ -1,7 +1,72 @@
-import { useParamsInt } from '@/hooks/useParamsInt';
+import { useLoaderData } from 'react-router-dom';
+import { HeaderOptions } from '@components/layout/HeaderOptions';
+import { ButtonIcon } from '@components/shared';
+import type { GameHistory } from '@types';
 
 export function GameHistory() {
-  const { idGame } = useParamsInt('idGame');
+  const scores = useLoaderData() as GameHistory[];
+  /**
+   * {
+   *   game: {
+   *     leader: true,
+   *     cities: false,
+   *   },
+   *   gameHistory: [{
+   *     ...gameHistory,
+   *     player: {
+   *       id,
+   *       avatar,
+   *       name
+   *     }
+   *   }],
+   * }
+   */
 
-  return <>GameHistory : {idGame}</>;
+  // Get player infos
+  // Get game infos
+  
+  return (
+    <main className="">
+      <HeaderOptions>
+        <ButtonIcon icon={() => <></>} aria-label="" />
+      </HeaderOptions>
+
+      {/* Score des joueurs */}
+      <section className='p-4'>
+        <div className="flex gap-4">
+          <div className="p-2 w-[100px] flex flex-col justify-center">
+            <div>&nbsp;</div>
+            <div>Military</div>
+            <div>Treasury</div>
+            <div>Wonders</div>
+            <div>Civilians</div>
+            <div>Scientifics</div>
+            <div>Commercials</div>
+            <div>Guilds</div>
+            <div>Armada</div>
+            <div>Leaders</div>
+            <div>Cities</div>
+            <div>Total</div>
+          </div>
+          <div className='flex gap-4 overflow-x-auto '>
+          {scores.map(score => (
+          <div key={score.idPlayer} className="w-[100px] flex flex-col items-center justify-center">
+            <div>Avatar</div>
+              <div>{score.military}</div>
+              <div>{score.treasury}</div>
+              <div>{score.wonders}</div>
+              <div>{score.civilians}</div>
+              <div>{score.scientifics}</div>
+              <div>{score.commercials}</div>
+              <div>{score.guilds}</div>
+              <div>{score.armada}</div>
+              <div>{score.leaders}</div>
+              <div>{score.cities}</div>
+              <div>{score.total}</div>
+          </div>
+          ))}</div>
+        </div>
+      </section>
+    </main>
+  );
 }
