860a91037ad0816e7fa62302d5279379b50e6e96
Forthtilliath <forth@live.fr>
2023-10-15 00:58:58 +0200

    :sparkles: feat: add FaDownload & FaImage

--- src/components/shared/Icons.tsx
+++ src/components/shared/Icons.tsx
@@ -409,3 +409,35 @@ export function BsPencilFill({ size = '1rem', ...props }: IconProps) {
     </svg>
   );
 }
+
+export function FaDownload({ size = '1rem', ...props }: IconProps) {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 512 512"
+      height={size}
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}>
+      <path d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z" />
+    </svg>
+  );
+}
+
+export function FaImage({ size = '1rem', ...props }: IconProps) {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 512 512"
+      height={size}
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}>
+      <path d="M464 448H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h416c26.51 0 48 21.49 48 48v288c0 26.51-21.49 48-48 48zM112 120c-30.928 0-56 25.072-56 56s25.072 56 56 56 56-25.072 56-56-25.072-56-56-56zM64 384h384V272l-87.515-87.515c-4.686-4.686-12.284-4.686-16.971 0L208 320l-55.515-55.515c-4.686-4.686-12.284-4.686-16.971 0L64 336v48z" />
+    </svg>
+  );
+}
