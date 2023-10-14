7629513686573cfdcaac1f286b85622f3fded355
Forthtilliath <forth@live.fr>
2023-10-14 13:38:01 +0200

    ✨ feat: add sum & avg

--- src/helpers/array.ts
+++ src/helpers/array.ts
@@ -55,3 +55,11 @@ export const getLabelAndPrevious: GetLabelAndPrevious = (pathname, routes) => {
 
   return { label, previous };
 };
+
+export function sum(arr: number[]): number {
+  return arr.reduce((total, n) => total + n, 0);
+}
+
+export function avg(arr: number[]): number {
+  return sum(arr) / arr.length;
+}
