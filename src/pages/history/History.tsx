93b73c338da5edefdf86430f7df94f751222b6cf
Forthtilliath <forth@live.fr>
2023-10-14 17:04:08 +0200

    :sparkles: feat: add listing of games

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -1,3 +1,38 @@
+import { useLoaderData } from 'react-router-dom';
+import type { GameHistoriesComplete } from '@types';
+import { HeaderOptions, Section } from '@components/layout';
+import { ButtonIcon } from '@components/shared';
+import { DisplayObject } from '@components/dev/DisplayObject';
+
 export default function History() {
-  return <main className="p-4">Blabla</main>;
+  const games = useLoaderData() as GameHistoriesComplete[];
+  console.log(games);
+
+  return (
+    <main>
+      <HeaderOptions>
+        <ButtonIcon icon={() => <></>} aria-label="" />
+      </HeaderOptions>
+
+      <Section>
+        <DisplayObject data={games} />
+        {games.map((gameHistories) => (
+          <div key={gameHistories.game.idGame}>
+            <header>
+              <h2>
+                {gameHistories.game.idGame} - {gameHistories.game.createdAt}
+              </h2>
+            </header>
+            <main>
+              {gameHistories.scores.map((score) => (
+                <div key={score.idPlayer}>
+                  {score.idPlayer} - {score.total}
+                </div>
+              ))}
+            </main>
+          </div>
+        ))}
+      </Section>
+    </main>
+  );
 }
