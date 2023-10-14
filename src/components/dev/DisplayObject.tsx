58dee34f5cf88446f80a42dcb948be9bb2d99835
Forthtilliath <forth@live.fr>
2023-10-14 23:12:42 +0200

    🔥 refactor: replace by react-json-view

--- src/components/dev/DisplayObject.tsx
+++
@@ -1,7 +0,0 @@
-type Props = {
-  data: ArrayLike<unknown> | Record<string, unknown>;
-};
-
-export function DisplayObject({ data }: Props) {
-  return <pre>{JSON.stringify(data, undefined, 2)}</pre>;
-}
