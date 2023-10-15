f54dac3ea501b147e4e101c8c6efb0f889f57c00
Forthtilliath <forth@live.fr>
2023-10-15 02:41:33 +0200

    💄 refactor: write className into 3 lines instead of 1

--- src/components/layout/HeaderOptions.tsx
+++ src/components/layout/HeaderOptions.tsx
@@ -1,8 +1,14 @@
 import { PropsWithChildren } from 'react';
+import { cn } from '@/helpers';
 
 export function HeaderOptions({ children }: PropsWithChildren) {
   return (
-    <div className="max-w-app pointer-events-none fixed -top-16 left-1/2 mx-auto flex h-16 w-full -translate-x-1/2 items-center justify-end pr-2 text-wonders-yellow">
+    <div
+      className={cn(
+        'fixed -top-16 left-1/2 mx-auto -translate-x-1/2 pr-2',
+        'max-w-app flex h-16 w-full items-center justify-end',
+        'pointer-events-none text-wonders-yellow '
+      )}>
       {children}
     </div>
   );
