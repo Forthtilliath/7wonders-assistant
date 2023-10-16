858022837e103b763124d39bee0b2ffc4cc551b4
Forthtilliath <forth@live.fr>
2023-10-16 14:39:29 +0200

    ✨ feat: add backend

--- src/lib/i18next.ts
+++ src/lib/i18next.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
 
 import LanguageDetector from 'i18next-browser-languagedetector';
 // don't want to use this?
@@ -10,6 +11,7 @@ i18n
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
+  .use(Backend)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
@@ -17,6 +19,7 @@ i18n
   .init({
     fallbackLng: 'en',
     debug: true,
+    
   });
 
 
