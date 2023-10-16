8364c96146f5d0783501597133eb46b720245298
Forthtilliath <forth@live.fr>
2023-10-17 00:01:02 +0200

    ✨ feat: add component

---
+++ src/components/charts/PieScorePlayer.tsx
@@ -0,0 +1,46 @@
+import { Pie } from 'react-chartjs-2';
+import { CATEG } from '@/constants';
+import { capitalize } from '@/helpers';
+import 'chart.js/auto';
+import { Scores } from '@types';
+
+type Props = {
+  scores: Scores;
+  extensions: string[];
+};
+
+export function PieScorePlayer({ scores, extensions }: Props) {
+  const [labels, data, backgroundColor, borderColor] = Object.entries(
+    scores
+  ).reduce(
+    (acc, [key, score]) => {
+      if (CATEG[key as Category].isExtension && !extensions.includes(key)) {
+        return acc;
+      }
+
+      acc[0].push(key);
+      acc[1].push(score);
+      acc[2].push(CATEG[key as Category].bgColor);
+      acc[3].push(CATEG[key as Category].borderColor);
+      return acc;
+    },
+    [[], [], [], []] as [string[], number[], string[], string[]]
+  );
+
+  console.log({ labels, data });
+
+  const pieData = {
+    labels: labels.map(capitalize),
+    datasets: [
+      {
+        label: 'Points',
+        data,
+        backgroundColor,
+        borderColor,
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return <Pie data={pieData} />;
+}
