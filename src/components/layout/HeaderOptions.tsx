a0a537038ef542f6acfaac00971400475ae70538
Forthtilliath <forth@live.fr>
2023-10-22 17:52:33 +0200

    🐛 fix: correct the hidden buttons

--- src/components/layout/HeaderOptions.tsx
+++ src/components/layout/HeaderOptions.tsx
@@ -5,9 +5,9 @@ export function HeaderOptions({ children }: PropsWithChildren) {
   return (
     <div
       className={cn(
-        'fixed -top-16 left-1/2 mx-auto -translate-x-1/2 pr-2',
+        'fixed top-0 left-1/2 mx-auto -translate-x-1/2 pr-2',
         'flex h-16 w-full max-w-app items-center justify-end',
-        'pointer-events-none text-wonders-yellow '
+        'pointer-events-none text-wonders-yellow z-1'
       )}>
       {children}
     </div>
