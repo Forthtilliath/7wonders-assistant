5035518de6abe414470e3b1f252ce7a064515bc9
Forthtilliath <forth@live.fr>
2023-10-23 23:18:39 +0200

    ✨ feat: add functional conversion btw versions

--- src/lib/convertDataVersion.ts
+++ src/lib/convertDataVersion.ts
@@ -1,52 +1,65 @@
-type Data_0_1_0 = {
-  settings: {
-    extensions: ('leaders' | 'cities' | 'babel' | 'armada' | 'edifice')[];
-    language: string;
-    version: string;
-  };
-  players: {
-    idPlayer: number;
-    name: string;
-    avatar: string;
-    isFavorite: boolean;
-    isArchived: boolean;
-  }[];
-  history: {
-    game: {
-      idGame: number;
-      createdAt: number;
-      extensions: ('leaders' | 'cities' | 'babel' | 'armada' | 'edifice')[];
-    };
-    scores: {
-      idGame: number;
-      idPlayer: number;
-      total: number;
-      ranking: number;
-      military: number;
-      treasury: number;
-      wonders: number;
-      civilians: number;
-      scientifics: number;
-      commercials: number;
-      guilds: number;
-      leaders?: number;
-      cities?: number;
-      armada?: number;
-    }[];
-  }[];
-};
+import { isVersion } from '@helpers';
+import { APP_CONST } from '@constants';
+import { Data, dataSchema } from './zodSchemas';
 
-export type DataVersion = Data_0_1_0;
+export type DataVersion = Data[keyof Data];
 
-export type DataLastVersion = Data_0_1_0;
+export type DataLastVersion = Data['0.3.0'];
 
 export function convertDataVersion<T extends DataVersion>(data: T) {
-  let version = data.settings.version;
+  const version = data.settings.version;
+  const result = dataSchema[version].safeParse(data);
 
-  if (version === '0.1.0') {
-    // convert to next version : 0.2.0
-    version = '0.2.0';
+  if (!result.success) {
+    return result;
   }
 
-  return data;
+  if (version === APP_CONST.version) {
+    return {
+      success: true,
+      data,
+    };
+  }
+
+  if (isVersion(data, '0.2.0')) {
+    const dataNextVersion: Data['0.3.0'] = {
+      ...data,
+      settings: { ...data.settings, version: '0.3.0' as const },
+      players: data.players.map((p) => {
+        return { ...p, isDeleted: false };
+      }),
+    };
+    return convertDataVersion(dataNextVersion);
+  }
+
+  if (isVersion(data, '0.1.0')) {
+    const dataNextVersion: Data['0.2.0'] = {
+      ...data,
+      settings: Object.assign(data.settings, { version: '0.2.0' as const }),
+      history: data.history.map((h) => {
+        const { idGame, createdAt, ...extensions } = h.game;
+        return {
+          ...h,
+          game: {
+            idGame,
+            createdAt,
+            extensions: Object.entries(extensions)
+              .filter(([, v]) => v)
+              .map(
+                ([k]) =>
+                  k.toLowerCase() as
+                    | 'leaders'
+                    | 'cities'
+                    | 'babel'
+                    | 'armada'
+                    | 'edifice'
+              ),
+          },
+        };
+      }),
+    };
+    return convertDataVersion(dataNextVersion);
+  }
+
+  return result;
 }
