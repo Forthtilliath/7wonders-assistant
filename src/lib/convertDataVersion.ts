0e6bd0a49695401c562fe2e8dda586c071d722ff
Forthtilliath <forth@live.fr>
2023-10-24 16:25:18 +0200

    🐛 fix: convert correctly old version into new version

--- src/lib/convertDataVersion.ts
+++ src/lib/convertDataVersion.ts
@@ -1,65 +1,80 @@
 import { isVersion } from '@helpers';
 import { APP_CONST } from '@constants';
-import { Data, dataSchema } from './zodSchemas';
+import { type Data, dataSchema } from './zodSchemas';
 
 export type DataVersion = Data[keyof Data];
 
-export type DataLastVersion = Data['0.3.0'];
+const lastSchemaVersion = dataSchema[APP_CONST.version];
 
-export function convertDataVersion<T extends DataVersion>(data: T) {
-  const version = data.settings.version;
-  const result = dataSchema[version].safeParse(data);
+export function convertDataVersion<T extends DataVersion>(
+  dataIn: T
+): ReturnType<typeof lastSchemaVersion.safeParse> {
+  const result = dataSchema[dataIn.settings.version].safeParse(dataIn);
 
   if (!result.success) {
     return result;
   }
 
-  if (version === APP_CONST.version) {
-    return {
-      success: true,
-      data,
-    };
+  let dataOut: DataVersion = dataIn;
+
+  if (isVersion(dataOut, '0.1.0')) {
+    console.log('Convert version 0.1.0 to version 0.2.0');
+    dataOut = convertDataFromVersion0_1_0To0_2_0(dataOut);
   }
 
-  if (isVersion(data, '0.2.0')) {
-    const dataNextVersion: Data['0.3.0'] = {
-      ...data,
-      settings: { ...data.settings, version: '0.3.0' as const },
-      players: data.players.map((p) => {
-        return { ...p, isDeleted: false };
-      }),
-    };
-    return convertDataVersion(dataNextVersion);
+  if (isVersion(dataOut, '0.2.0')) {
+    console.log('Convert version 0.2.0 to version 0.3.0');
+    dataOut = convertDataFromVersion0_2_0To0_3_0(dataOut);
   }
 
-  if (isVersion(data, '0.1.0')) {
-    const dataNextVersion: Data['0.2.0'] = {
-      ...data,
-      settings: Object.assign(data.settings, { version: '0.2.0' as const }),
-      history: data.history.map((h) => {
-        const { idGame, createdAt, ...extensions } = h.game;
-        return {
-          ...h,
-          game: {
-            idGame,
-            createdAt,
-            extensions: Object.entries(extensions)
-              .filter(([, v]) => v)
-              .map(
-                ([k]) =>
-                  k.toLowerCase() as
-                    | 'leaders'
-                    | 'cities'
-                    | 'babel'
-                    | 'armada'
-                    | 'edifice'
-              ),
-          },
-        };
-      }),
-    };
-    return convertDataVersion(dataNextVersion);
-  }
+  return dataSchema[APP_CONST.version].safeParse(dataOut);
+}
 
-  return result;
+function convertDataFromVersion0_2_0To0_3_0(
+  dataIn: Data['0.2.0']
+): Data['0.3.0'] {
+  const dataNextVersion: Data['0.3.0'] = {
+    ...dataIn,
+    settings: { ...dataIn.settings, version: '0.3.0' as const },
+    players: dataIn.players.map((p) => {
+      return { ...p, isDeleted: false };
+    }),
+  };
+
+  return dataNextVersion;
+}
+
+function convertDataFromVersion0_1_0To0_2_0(
+  dataIn: Data['0.1.0']
+): Data['0.2.0'] {
+  const dataNextVersion: Data['0.2.0'] = {
+    ...dataIn,
+    settings: {
+      ...dataIn.settings,
+      version: '0.2.0' as const,
+    },
+    history: dataIn.history.map((h) => {
+      const { idGame, createdAt, ...extensions } = h.game;
+      return {
+        ...h,
+        game: {
+          idGame,
+          createdAt,
+          extensions: Object.entries(extensions)
+            .filter(([, v]) => v)
+            .map(
+              ([k]) =>
+                k.toLowerCase() as
+                  | 'leaders'
+                  | 'cities'
+                  | 'babel'
+                  | 'armada'
+                  | 'edifice'
+            ),
+        },
+      };
+    }),
+  };
+
+  return dataNextVersion;
 }
