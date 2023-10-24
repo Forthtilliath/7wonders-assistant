040198d4ba370c142bea80010a06cec66947ec81
Forthtilliath <forth@live.fr>
2023-10-24 16:20:46 +0200

    🏷️ type: add CurrentVersion

--- src/@types/constantes.d.ts
+++ src/@types/constantes.d.ts
@@ -1,8 +1,9 @@
-import { CATEGORIES, EXTENSIONS } from '@constants';
+import { APP_CONST, CATEGORIES, EXTENSIONS } from '@constants';
 
 declare global {
   type Extension = (typeof EXTENSIONS)[number];
   type Category = (typeof CATEGORIES)[number];
+  type CurrentVersion = APP_CONST.version
 }
 
 export {};
