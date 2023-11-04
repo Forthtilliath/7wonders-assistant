c83378fe054a970fddbb444437d3fc0a5f819b12
Mickael Rebeau <rebeau.mickael@gmail.com>
2023-11-04 05:53:54 +0100

    ✨ feat: add capacitor test save

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,9 +1,24 @@
+import { ButtonSettings } from '@/components/ui/ButtonSettings';
+import { Directory, Filesystem, Encoding } from '@capacitor/filesystem';
 import { Section } from '@components/layout';
 
+
 export default function Statistics() {
+ const saveFile = async (fileName: string, data: string) => {
+   await Filesystem.writeFile({
+     path: fileName,
+     data: data,
+     directory: Directory.Documents,
+     encoding: Encoding.UTF8,
+   });
+ };
+  
   return (
     <main>
-      <Section>Statistics</Section>
+      <Section>
+        <h1>Statistics</h1>
+        <ButtonSettings onClick={() => saveFile('users.json', JSON.stringify({"name": "John"}))}>Save</ButtonSettings>
+      </Section>
     </main>
   );
 }
