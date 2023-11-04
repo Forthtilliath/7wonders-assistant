e41214258a92a9227b2f9d38eead9e3b56732a92
Forthtilliath <forth@live.fr>
2023-11-04 04:44:58 +0100

    🔥 feat: remove constant use

--- src/__tests__/utils.ts
+++ src/__tests__/utils.ts
@@ -1,7 +1,5 @@
 import type { Player } from '@types';
-import { LS_KEY } from '@constants';
-
 export function getPlayers(): Player[] {
-  const LS = localStorage.getItem(LS_KEY.players);
+  const LS = localStorage.getItem('players');
   return LS ? JSON.parse(LS) : [];
 }
