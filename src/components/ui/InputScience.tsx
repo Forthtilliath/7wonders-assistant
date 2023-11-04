cdda8b7e0784e54aa440d3513af1515e54ba6718
Forthtilliath <forth@live.fr>
2023-11-04 04:28:56 +0100

    ✨ feat: update component

--- src/components/ui/InputScience.tsx
+++ src/components/ui/InputScience.tsx
@@ -1,16 +1,19 @@
-type Props = {
-  img: string
-}
+import { ComponentPropsWithoutRef } from 'react';
 
-export function InputScience({img}: Props) {
+type Props = ComponentPropsWithoutRef<'input'> & {
+  img: string;
+};
+
+export function InputScience({ img, ...inputProps }: Props) {
   return (
     <div className="mb-4 flex gap-4">
-    <img src={img} alt="Tablet" className="w-16" />
-    <input
-      type="number"
-      className="border-wonders-dark w-fit rounded border-2 bg-slate-900 p-3 text-center"
-      defaultValue="0"
-    />
-  </div>
-  )
+      <img src={img} alt="Tablet" className="w-16" />
+      <input
+        type="number"
+        className="border-wonders-dark w-full rounded border-2 bg-slate-900 p-3 text-center"
+        defaultValue={0}
+        {...inputProps}
+      />
+    </div>
+  );
 }
