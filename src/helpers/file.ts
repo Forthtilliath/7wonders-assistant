e757c40be8051bb4e2b3704beaebf0e27a6883e2
Forthtilliath <forth@live.fr>
2023-11-06 18:29:43 +0100

    ✨ feat: add verification of fileName at saveFile

--- src/helpers/file.ts
+++ src/helpers/file.ts
@@ -1,10 +1,30 @@
-import { Directory, Encoding, Filesystem } from "@capacitor/filesystem";
+import { Directory, Encoding, Filesystem } from '@capacitor/filesystem';
 
-export async function saveFile (fileName: string, data: Record<string, unknown>)  {
-  await Filesystem.writeFile({
-    path: fileName,
-    data: JSON.stringify(data),
-    directory: Directory.Documents,
-    encoding: Encoding.UTF8,
-  });
+/**
+ * Saves data to a file.
+ *
+ * @param fileName - The name of the file to be saved.
+ * @param data - The data to be written to the file.
+ * @throws {Error} If the file name is invalid or if there is an error during the file writing process.
+ */
+export async function saveFile(
+  fileName: string,
+  data: Record<string, unknown> | Array<unknown>
+) {
+  const regex = /^[\w,\s-]+\.[a-zA-Z0-9]{2,4}$/;
+
+  if (!regex.test(fileName) || fileName.length > 25) {
+    throw new Error('File name is invalid!');
+  }
+  
+  try {
+    await Filesystem.writeFile({
+      path: fileName.trim(),
+      data: JSON.stringify(data),
+      directory: Directory.Documents,
+      encoding: Encoding.UTF8,
+    });
+  } catch (error) {
+    throw new Error('Failed to save file');
+  }
 }
