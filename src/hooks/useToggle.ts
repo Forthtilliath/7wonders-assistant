3d47f876a802ceae3c5de08ffba8a4d475e9708c
Forthtilliath <forth@live.fr>
2023-11-06 23:43:42 +0100

    📝 doc: add jsdoc

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,9 +1,19 @@
-import { Dispatch, SetStateAction, useCallback, useState } from "react";
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 
+/**
+ * Custom hook that returns an array with three elements: the current value of a boolean state,
+ * a function to toggle the state value, and a function to update the state value.
+ *
+ * @param defaultValue - The initial value of the boolean state. Defaults to `false`.
+ * @returns An array with three elements:
+ *   - The current value of the boolean state.
+ *   - A function to toggle the state value.
+ *   - A function to update the state value.
+ */
 export function useToggle(
-  defaultValue?: boolean
+  defaultValue = false
 ): [boolean, () => void, Dispatch<SetStateAction<boolean>>] {
-  const [value, setValue] = useState(!!defaultValue);
+  const [value, setValue] = useState(defaultValue);
 
   const toggle = useCallback(() => setValue((x) => !x), []);
 
