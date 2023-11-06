9a89879170fc608a200c8b86a9cc66da87ee2149
Forthtilliath <forth@live.fr>
2023-11-07 00:35:16 +0100

    📝 doc: add jsdoc

--- src/hooks/useParamsInt.ts
+++ src/hooks/useParamsInt.ts
@@ -1,5 +1,18 @@
 import { useParams } from 'react-router-dom';
 
+/**
+ * Parses the specified key or keys from the URL parameters and returns an object with the parsed integer values.
+ * If the key or any of the keys are not found in the URL parameters, an error is thrown.
+ * @param key - The key or keys to extract from the URL parameters.
+ * @returns An object with the same keys as the input key (or keys in the array), but with their corresponding values parsed as integers.
+ * @throws Error if the key or any of the keys are not found in the URL parameters.
+ * @example
+ * const params = useParamsInt('id');
+ * console.log(params); // { id: 123 }
+ *
+ * const paramsArray = useParamsInt(['id', 'page']);
+ * console.log(paramsArray); // { id: 123, page: 1 }
+ */
 export function useParamsInt<K extends string>(
   key: K | K[]
 ): Record<K, number> {
