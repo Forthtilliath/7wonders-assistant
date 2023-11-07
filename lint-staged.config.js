c9f93d66e5e5c1253b1b711c3d8e2d4537230175
Forthtilliath <forth@live.fr>
2023-11-07 23:01:11 +0100

    🔨 build: remove pnpx

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,13 @@
 export default {
   // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'pnpx tsc --noEmit',
+  '**/*.(ts|tsx)': () => 'tsc --noEmit',
 
   // Lint then format TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
-    `pnpx eslint --fix ${filenames.join(' ')}`,
-    `pnpx prettier --write ${filenames.join(' ')}`,
+    `eslint --fix ${filenames.join(' ')}`,
+    `prettier --write ${filenames.join(' ')}`,
   ],
 
   // Format MarkDown and JSON
-  '**/*.(md|json)': (filenames) =>
-    `pnpx prettier --write ${filenames.join(' ')}`,
+  '**/*.(md|json)': (filenames) => `prettier --write ${filenames.join(' ')}`,
 };
