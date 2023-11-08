4ae37b3f587700a4b9a916de003fbb8ebf5ea431
Forthtilliath <forth@live.fr>
2023-11-08 23:03:57 +0100

    ✨ feat: reset extensions in resetGame()

--- src/lib/stores/gameStore.ts
+++ src/lib/stores/gameStore.ts
@@ -60,6 +60,7 @@ export const useGameStore = create<State>()(
         resetGame: () =>
           set(
             {
+              extensions: [],
               players: [],
               scores: {},
             },
