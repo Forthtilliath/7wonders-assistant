1a5f5c548823fba1a5dd9b88dce3690b08bf88e6
Forthtilliath <forth@live.fr>
2023-11-23 00:41:44 +0100

    ✨ feat: add data table

--- src/components/charts/TablePlayersWinRate.tsx
+++ src/components/charts/TablePlayersWinRate.tsx
@@ -1,73 +1,67 @@
-import { Fragment } from 'react';
+import { Fragment, PropsWithChildren } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import { loaderStatistics } from '@/lib/loaders';
+import { getPlayersStats } from '@/lib/utils/statistics';
+import { CardPlayer } from '../cards';
 
 export function TablePlayersWinRate() {
+  const { t } = useTranslation(undefined, { keyPrefix: 'statistics' });
   const { games, players } = useLoaderData() as LoaderData<
     typeof loaderStatistics
   >;
-  const { t } = useTranslation(undefined, { keyPrefix: 'statistics' });
 
-  /**
-   * {
-   *   idPlayer: {
-   *     wins: number,
-   *     played: number
-   *   }
-   * }
-   */
-
-  const playersStat = games.reduce(
-    (stats, { scores }) => {
-      //
-      console.log(scores); //array : idPlayer & ranking
-      scores.forEach(({ idPlayer, ranking }) => {
-        if (!stats[idPlayer]) {
-          stats[idPlayer] = {
-            wins: 0,
-            played: 0,
-          };
-        }
-        if (ranking) {
-          stats[idPlayer].wins++;
-        }
-        stats[idPlayer].played++;
-      });
-
-      return stats;
-    },
-    {} as Record<number, { wins: number; played: number }>
-  );
-  console.log(playersStat);
-
-  // useEffect(() => {
-  //   console.log(players)
-  //   console.log(games)
-  // }, [games, players])
-
-  /**
-   * TODO :
-   * - [x] Récupérer les parties
-   * - [ ] Les rassembler par joueur
-   * - [ ] Compter le nombre de parties que chaque joueur a fini premier
-   * - [ ] Compter le nombre de parties que chaque joueur a joué
-   */
+  const playersStats = getPlayersStats(games);
 
   return (
-    <div className="grid grid-cols-4">
-      <div className="col-span-2">{t('name')}</div>
-      <div>{t('win_rate')}</div>
-      <div>{t('games')}</div>
+    <div className="grid grid-cols-3 [&>*:nth-child(6n+1)]:bg-white/5 [&>*:nth-child(6n+2)]:bg-white/5 [&>*:nth-child(6n+3)]:bg-white/5">
+      <THead>{t('name')}</THead>
+      <THead>{t('win_rate')}</THead>
+      <THead>{t('games')}</THead>
 
       {players.map((player) => (
         <Fragment key={player.idPlayer}>
-          <div></div>
-          <div>{player.name}</div>
-          <div>100%</div>
-          <div>15</div>
+          <TBody>
+            <Link to={`/statistics/${player.idPlayer}`}>
+              <CardPlayer
+                {...player}
+                className="aspect-square h-full max-w-[50px]"
+                classNameH2="text-xs font-normal"
+              />
+            </Link>
+          </TBody>
+          {playersStats[player.idPlayer] ? (
+            <>
+              <TBody>
+                {Math.round(
+                  (playersStats[player.idPlayer].wins /
+                    playersStats[player.idPlayer].played) *
+                    100
+                )}
+                %
+              </TBody>
+              <TBody>{playersStats[player.idPlayer].played}</TBody>
+            </>
+          ) : (
+            <>
+              <TBody>0%</TBody>
+              <TBody>0</TBody>
+            </>
+          )}
         </Fragment>
       ))}
     </div>
   );
+}
+
+function THead({ children }: PropsWithChildren) {
+  return (
+    <div className="flex items-center justify-center p-2 font-bold">
+      {children}
+    </div>
+  );
+}
+
+function TBody({ children }: PropsWithChildren) {
+  return <div className="flex items-center justify-center">{children}</div>;
 }
